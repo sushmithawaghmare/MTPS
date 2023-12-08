e98d6dca45a865421f30861f30ef136419016f2c
sushmithawaghmare <sushmithawaghmare@gmail.com>
2023-12-08 10:40:47 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+import pandas as pd
+import joblib
+
+app = Flask(__name__)
+model = joblib.load('random_forest_model_n.joblib')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    sequence = request.form['sequence']
+    new_data = {'Sequence': [sequence]}
+    new_df = pd.DataFrame(new_data)
+    prediction = model.predict(new_df)
+    return render_template('predict.html', sequence=sequence, prediction=prediction)
+
+if __name__ == '__main__':
+    app.run(debug=True)
