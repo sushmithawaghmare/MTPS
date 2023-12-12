4785c7ed0aef7587fcf95fb5fd3d407126f238d6
sushmithawaghmare <140073784+sushmithawaghmare@users.noreply.github.com>
2023-12-12 12:31:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,4 +18,4 @@ def predict():
     return render_template('predict.html', sequence=sequence, prediction=prediction)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0",port=5000)
